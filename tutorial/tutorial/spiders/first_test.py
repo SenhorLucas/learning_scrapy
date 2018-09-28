4ffc1dae08818bed3331f1accc6b238aa6bc1ebb
Lucas Viana <lucas.nviana@gmail.com>
2018-09-28 16:58:44 +0200

    first project set up

---
+++ tutorial/tutorial/spiders/first_test.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = "blogspider"
+
+    def start_requests(self):
+        urls = ["https://www.webscraper.io/test-sites/tables"]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url
+        filename = "spider1.html"
+        with open(filename, "wb") as f:
+            f.write(response.body)
+        self.log("Saved file %s" % filename)
